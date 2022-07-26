0b66ca7b51da144b7c88b4ad86a18e9ec85a7dfb
Cristian David Acevedo Posada <cacevedo@serempre.com>
2022-07-25 23:07:21 -0500

    feat(main): Prepare main page

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import './style.css';
 import typescriptLogo from './typescript.svg';
 
-import './exercise/exercise.ts';
-
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img id="ts-logo" src="${typescriptLogo}" class="logo" alt="TypeScript logo" />
     </a>
-    <h1>Open the browser console</h1>
+    <h1>Welcome to Typescript workshop</h1>
     <p class="read-the-docs">
-      Click on the TypeScript logos to learn more
+      Wait for the exhibitor's instructions
     </p>
   </div>
 `;
